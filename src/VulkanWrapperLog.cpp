a6863a4961237afb0abf6809ee1ecfb36b3fe54e
B Marques <brunosmarques1995@hotmail.com>
2022-12-26 21:40:46 -0300

    Added the correct namespace to GetNumberOfExtensions

--- src/VulkanWrapperLog.cpp
+++ src/VulkanWrapperLog.cpp
@@ -1,7 +1,7 @@
 #include "VulkanWrapperLog.hpp"
 #include <vulkan/vulkan.h>
 
-uint32_t GetNumberOfExtensions()
+uint32_t VulkanWrapper::GetNumberOfExtensions()
 {
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
